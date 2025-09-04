293c610ea36023387fdacf8bb49403bd8b2ff37f
kusuma_.__ <rhshah199@gmail.com>
2025-09-03 21:45:47 -0700

    added video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {CardComponent} from './components/card/card.component'; 
 const routes: Routes = [
     {path: '', component: HomeComponent}
 ];
