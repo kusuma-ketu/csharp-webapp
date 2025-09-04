579a82f37f5a596996b7f1f15555e44805d8859e
kusuma_.__ <rhshah199@gmail.com>
2025-09-03 21:03:31 -0700

    fixed routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',       // HTML tag used in index.html
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule]
 })
 export class AppComponent {
   title = 'My Angular App';
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './app/pages/home/home.component';
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      {path: '', component: HomeComponent}
+    ])
+  ]
+})
   .catch((err) => console.error(err));
